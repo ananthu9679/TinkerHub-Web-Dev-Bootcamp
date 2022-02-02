b7c83980d78f516f57c09460d0f338c4ba30ef8c
Ananthakrishnan k s <56349057+ananthu9679@users.noreply.github.com>
2022-02-02 16:11:14 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,26 @@
+function changetored() {
+   
+                           document.body.style.backgroundColor = "red";
+
+}               
+   
+
+function changetoorange() {
+    
+    
+                         document.body.style.backgroundColor = "orange";
+}
+
+function changetoblue() {
+    
+                          document.body.style.backgroundColor = "blue";
+}
+
+
+ function show() {
+     
+    
+    document.getElementById('image').style.display = "block";
+
+     document.getElementById('btnid');
+ }
